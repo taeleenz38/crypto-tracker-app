7f47d4f70a85922222062c0c7fe7988b501d20d1
Tae Lee <taekyu.lee3@gmail.com>
2023-07-07 23:14:10 +1000

    header css (tailwind)

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Coins.css'
 
 const CoinItem = (props) => {
     return (
--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import CoinItem from './CoinItem'
+import './Coins.css'
+
 
 const Coins = (props) => {
     return (
-        <div className='container'>
+        <div class="w-5/6 m-auto">
             <div>
-                <div className="heading">
+                <div class="flex justify-between items-center mx-5 my-3 py-1 border-white border-b-2 font-bold">
                     <p>#</p>
                     <p className='coin-name'>Coin</p>
                     <p>Price</p>
                     <p>24h</p>
-                    <p class="visible md:invisible">Volume</p>
-                    <p class="visible md:invisible">Mkt Cap</p>
+                    <p class="invisible md:visible">Volume</p>
+                    <p class="invisible md:visible">Mkt Cap</p>
                 </div>
 
                 {props.coins.map(coins => {
