3a77d052e9baf14285f76791e919fd53997fb8db
Tae Lee <taekyu.lee3@gmail.com>
2023-07-07 23:03:19 +1000

    general body styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         <div>
             <div className='navbar'>
                 <FaCoins className='icon' />
-                <h1> Coin <span className='purple'>Search</span></h1>
+                <h1>Coin Search</h1>
             </div>
         </div>
     )
