5e67787aa8229d459d73207489e39556e1299473
Tae Lee <taekyu.lee3@gmail.com>
2023-07-07 22:58:07 +1000

    navbar styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Coins from './components/Coins'
+import Navbar from './components/Navbar'
 
 function App() {
+  const [coins, setCoins] = useState([]);
 
-  const [ coins, setCoins ] = useState([])
-
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en'
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en";
 
   useEffect(() => {
-    axios.get(url).then((response) => {
-      setCoins(response.data)
-      console.log(response.data[0])
-    }).catch((error) => {
-      console.log(error)
-    })
-  }, [])
+    axios
+      .get(url)
+      .then((response) => {
+        setCoins(response.data);
+        console.log(response.data[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <>
-    hi
+    <Navbar />
+      <Coins coins={coins} />
     </>
   );
 }
---
+++ src/components/CoinItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const CoinItem = (props) => {
+    return (
+        <div className='coin-row'>
+            <p>{props.coins.market_cap_rank}</p>
+            <div className="img-symbol">
+                <img src={props.coins.image} alt="coin-logo" />
+                <p>{props.coins.symbol}</p>
+            </div>
+            <p>{props.coins.current_price}</p>
+            <p>{props.coins.price_change_percentage_24h}</p>
+            <p class="visible md:invisible">{props.total_volume}</p>
+            <p class="visible md:invisible">{props.coins.market_cap}</p>
+
+        </div>
+    )
+}
+
+export default CoinItem
