2786059e994a9e49e55f583672e6b8be3a32aac5
Tae Lee <taekyu.lee3@gmail.com>
2023-07-12 22:07:06 +1000

    api WIP

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Coins coins={coins} />} />
-        <Route path="/coindescription" element={<Coin />}>
+        <Route path="/coin" element={<Coin />}>
           <Route path=":coinId" element={<Coin />} />
         </Route>
       </Routes>
--- src/routes/Coin.jsx
+++ src/routes/Coin.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'  
+import React, { useState, useEffect } from 'react'
+
 
 const Coin = () => {
+
+  const params = useParams()
+  const [coin, setCoin] = useState({})
+
+  const url = `"https://api.coingecko.com/api/v3/coins/${params.coinId}"`
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoin(response.data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
   return (
     <div>
-      
+      <h1>{coin.id}</h1>
     </div>
   )
 }
