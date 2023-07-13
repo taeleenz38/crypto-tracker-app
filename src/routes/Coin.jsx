52b017fa7e8674c4ba2d9ca58170ac2a3a2eb33a
Tae Lee <taekyu.lee3@gmail.com>
2023-07-13 13:53:09 +1000

    table done

--- src/routes/Coin.jsx
+++ src/routes/Coin.jsx
@@ -21,42 +21,42 @@ const Coin = () => {
   return (
     <div>
       <div>
-        <div class="w-1/2 mx-auto">
-          <div class="text-md my-3">
+        <div class="w-6/12 mx-auto">
+          <div class="text-md my-5">
             <span class=" border-2 border-blue-700 bg-blue-700 rounded-md">Rank # {coin.market_cap_rank}</span>
           </div>
           <div class="flex items-center justify-between">
-            <div class="flex items-center">
+            <div class="flex items-center text-xl">
               {coin.image ? <img class="mr-2" src={coin.image.small} alt='' /> : null}
               <p class="mr-2">{coin.name}</p>
               {coin.symbol ? <p>{coin.symbol.toUpperCase()}/USD</p> : null}
             </div>
             <div>
-              {coin.market_data?.current_price ? <h1>${coin.market_data.current_price.usd.toLocaleString()}</h1> : null}
+              {coin.market_data?.current_price ? <h1 class="text-2xl font-bold">${coin.market_data.current_price.usd.toLocaleString()}</h1> : null}
             </div>
           </div>
         </div>
 
-        <div>
-          <table>
-            <thead>
+        <div class="w-6/12 mx-auto text-center mt-8 border-2 rounded-xl">
+          <table class="mx-auto">
+            <thead class="border-b-2">
               <tr>
-                <th>1h</th>
-                <th>24h</th>
-                <th>7d</th>
-                <th>14d</th>
-                <th>30d</th>
-                <th>1y</th>
+                <th class="w-36 py-4">1h</th>
+                <th class="w-36 py-4">24h</th>
+                <th class="w-36 py-4">7d</th>
+                <th class="w-36 py-4">14d</th>
+                <th class="w-36 py-4">30d</th>
+                <th class="w-36 py-4">1y</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td>{coin.market_data?.price_change_percentage_1h_in_currency ? <p>{coin.market_data.price_change_percentage_1h_in_currency.usd.toFixed(1)}%</p> : null}</td>
-                <td>{coin.market_data?.price_change_percentage_24h_in_currency ? <p>{coin.market_data.price_change_percentage_24h_in_currency.usd.toFixed(1)}%</p> : null}</td>
-                <td>{coin.market_data?.price_change_percentage_24h_in_currency ? <p>{coin.market_data.price_change_percentage_7d_in_currency.usd.toFixed(1)}%</p> : null}</td>
-                <td>{coin.market_data?.price_change_percentage_24h_in_currency ? <p>{coin.market_data.price_change_percentage_14d_in_currency.usd.toFixed(1)}%</p> : null}</td>
-                <td>{coin.market_data?.price_change_percentage_24h_in_currency ? <p>{coin.market_data.price_change_percentage_30d_in_currency.usd.toFixed(1)}%</p> : null}</td>
-                <td>{coin.market_data?.price_change_percentage_24h_in_currency ? <p>{coin.market_data.price_change_percentage_1y_in_currency.usd.toFixed(1)}%</p> : null}</td>
+                <td class="w-36 py-4">{coin.market_data?.price_change_percentage_1h_in_currency ? <p>{coin.market_data.price_change_percentage_1h_in_currency.usd.toFixed(1)}%</p> : null}</td>
+                <td class="w-36 py-4">{coin.market_data?.price_change_percentage_24h_in_currency ? <p>{coin.market_data.price_change_percentage_24h_in_currency.usd.toFixed(1)}%</p> : null}</td>
+                <td class="w-36 py-4">{coin.market_data?.price_change_percentage_24h_in_currency ? <p>{coin.market_data.price_change_percentage_7d_in_currency.usd.toFixed(1)}%</p> : null}</td>
+                <td class="w-36 py-4">{coin.market_data?.price_change_percentage_24h_in_currency ? <p>{coin.market_data.price_change_percentage_14d_in_currency.usd.toFixed(1)}%</p> : null}</td>
+                <td class="w-36 py-4">{coin.market_data?.price_change_percentage_24h_in_currency ? <p>{coin.market_data.price_change_percentage_30d_in_currency.usd.toFixed(1)}%</p> : null}</td>
+                <td class="w-36 py-4">{coin.market_data?.price_change_percentage_24h_in_currency ? <p>{coin.market_data.price_change_percentage_1y_in_currency.usd.toFixed(1)}%</p> : null}</td>
               </tr>
             </tbody>
           </table>
