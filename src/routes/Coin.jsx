9c8afcffa33223ce7ba243f8a81d379438c9e783
Tae Lee <taekyu.lee3@gmail.com>
2023-07-13 13:59:03 +1000

    DOMPURIFY

--- src/routes/Coin.jsx
+++ src/routes/Coin.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
 import React, { useState, useEffect } from 'react'
+import DOMPurify from 'dompurify'
+
 
 
 const Coin = () => {
@@ -61,6 +63,45 @@ const Coin = () => {
             </tbody>
           </table>
         </div>
+
+        <div className='content'>
+          <div className='stats'>
+            <div className='left'>
+              <div className='row'>
+                <h4>24 Hour Low</h4>
+                {coin.market_data?.low_24h ? <p>${coin.market_data.low_24h.usd.toLocaleString()}</p> : null}
+              </div>
+              <div className='row'>
+                <h4>24 Hour High</h4>
+                {coin.market_data?.high_24h ? <p>${coin.market_data.high_24h.usd.toLocaleString()}</p> : null}                            </div>
+
+            </div>
+            <div className='right'>
+              <div className='row'>
+                <h4>Market Cap</h4>
+                {coin.market_data?.market_cap ? <p>${coin.market_data.market_cap.usd.toLocaleString()}</p> : null}
+              </div>
+              <div className='row'>
+                <h4>Circulating Supply</h4>
+                {coin.market_data ? <p>{coin.market_data.circulating_supply}</p> : null}
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+        <div className='content'>
+          <div className='about'>
+            <h3>About</h3>
+            <p dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(coin.description ? coin.description.en : ''),
+            }}>
+
+            </p>
+
+          </div>
+        </div>
+
 
       </div>
     </div>
